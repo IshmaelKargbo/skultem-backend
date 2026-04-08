933317481ac693e2ee7679883a840cf8ffbb08ae
Dev Isho <ishmaelkargbo5@outlook.com>
2026-04-08 16:40:31 +0000

    fix

--- src/main/java/com/moriba/skultem/application/mapper/AssessmentScoreMapper.java
+++ src/main/java/com/moriba/skultem/application/mapper/AssessmentScoreMapper.java
@@ -54,7 +54,7 @@ public class AssessmentScoreMapper {
     public static AssessmentScoreDTO toDTO(AssessmentScore param, String grade) {
         int score = 0, weight = 0, weightedScore = 0;
 
-        if (param.isApproved() || param.isSubmited() || grade != "N/A") {
+        if (param.isApproved() || param.isSubmited() || param.isCompleted()) {
             score = param.getScore();
             weight = param.getWeight();
             weightedScore = param.getWeightedScore();
