6ad6475a493e0e0ccbd488f24a962770bca337af
Dev Isho <ishmaelkargbo5@outlook.com>
2026-05-03 17:39:37 +0000

    fix cors

--- src/main/java/com/moriba/skultem/config/SecurityConfig.java
+++ src/main/java/com/moriba/skultem/config/SecurityConfig.java
@@ -58,7 +58,7 @@ public class SecurityConfig {
         public CorsConfigurationSource corsConfigurationSource() {
                 CorsConfiguration configuration = new CorsConfiguration();
                 configuration.setAllowedOrigins(
-                                List.of("http://localhost:3000", "https://skultem-app.vercel.app"));
+                                List.of("http://localhost:3000", "https://www.skultem.space"));
                 configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                 configuration.setAllowedHeaders(Arrays.asList(
                                 "Content-Type",
